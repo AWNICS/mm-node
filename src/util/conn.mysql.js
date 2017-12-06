a29f82ad5154363df56ed53817293a879cf770cd
niluroy <nilu.kumari@awnics.com>
2017-12-06 18:11:01 +0530

    separated connection in conn.mysql file

--- src/util/conn.mysql.js
+++ src/util/conn.mysql.js
@@ -1,33 +1,19 @@
-//import dotenv from 'dotenv';
+import Sequelize from 'sequelize';
 
-var mysql = require('mysql');
-var connection;
-//dotenv.config({ path: '.env.dev' });
+var sequelize = new Sequelize('test', 'arun', 'password', {
+    host: 'localhost',
+    dialect: 'mysql',
+    operatorsAliases: false,
+    logging: false
+});
 
-module.exports = {
-    /*
-        getting the sequelize connection
-    */
-    getConnection: () => {
-        const Sequelize = require('sequelize');
-        const sequelize = new Sequelize('test', 'arun', 'password', {
-            host: 'localhost',
-            dialect: 'mysql',
-            operatorsAliases: false,
-            logging: false //for removing default sequelize statements
-        })
+sequelize = sequelize;
 
-        /*
-            connection test
-        */
-        .authenticate()
-            .then(() => {
-                console.log('Connection has been established successfully.');
-            })
-            .catch(err => {
-                console.error('Unable to connect to the database:', err);
-            });
+/**
+ * connection authentication
+ */
+sequelize.authenticate()
+    .then(() => console.log('connection successfull for sequelize'))
+    .catch(error => console.error('error in connection: ', error));
 
-        return sequelize;
-    }
-};
+module.exports = sequelize;
