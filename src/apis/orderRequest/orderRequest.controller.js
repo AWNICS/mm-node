890c51130afb89d21a795dd3994847ccf839bee1
lenkaswarna <swarnakshyari.lenka@awnics.com>
2017-12-29 17:01:16 +0530

    swagger updated for orderRequest and specilitiy

--- src/apis/orderRequest/orderRequest.controller.js
+++ src/apis/orderRequest/orderRequest.controller.js
@@ -5,30 +5,160 @@ import log from '../../config/log4js.config';
 var orderRequestsService = new OrderRequestsService();
 var router = express.Router();
 
+/**
+ * @swagger
+ * definitions:
+ *   OrderRequest:
+ *     properties:
+ *       id:
+ *         type: string
+ *         format: date
+ *       tel:
+ *         type: string
+ *       fullname:
+ *         type: string
+ *       watel:
+ *         type: string
+ *       mail:
+ *         type: string
+ *       uFile:
+ *         type: integer
+ *       manual:
+ *         type: string
+ *       termsAccepted:
+ *         type: true
+ *       confirmationId:
+ *         type: string
+ *       location:
+ *         type: string
+ *       waitingTime:
+ *         type: integer
+ *       dp:
+ *         type: integer
+ *       button:
+ *         type: string
+ *       speciality:
+ *         type: string
+ */
+/**
+ * @swagger
+ * /orderRequest/controllers/createOrderRequest:
+ *   post:
+ *     tags:
+ *       - OrderRequest
+ *     description: Creates a new orderRequest
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: OrderRequest
+ *         description: orderRequest object
+ *         in: body
+ *         required: true
+ *         schema:
+ *           $ref: '#/definitions/OrderRequest'
+ *     responses:
+ *       200:
+ *         description: Successfully created
+ */
 router.post('/controllers/createOrderRequest', (req, res) => {
     orderRequestsService.createOrderRequest(req.body, (result) => {
         res.json(result);
     });
 });
 
+/**
+ * @swagger
+ * /orderRequest/controllers/readAllOrderRequests:
+ *   get:
+ *     tags:
+ *       - OrderRequest
+ *     description: Returns all orderRequest
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: An array of orderRequest
+ *         schema:
+ *           $ref: '#/definitions/OrderRequest'
+ */
 router.get('/controllers/readAllOrderRequests', (req, res) => {
     orderRequestsService.readAllOrderRequests((results) => {
         res.json(results);
     });
 });
 
+/**
+ * @swagger
+ * /orderRequest/controllers/readOrderRequestById/{id}:
+ *   get:
+ *     tags:
+ *       - OrderRequest
+ *     description: Returns orderRequest by id
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         description: id for orderRequest to return
+ *         required: true
+ *         type: string
+ *         schema:
+ *           $ref: '#/definitions/OrderRequest'
+ *     responses:
+ *       200:
+ *         description: An orderRequest return
+ */
 router.get('/controllers/readOrderRequestById/:id', (req, res) => {
     orderRequestsService.readOrderRequestById(req.params.id, (result) => {
         res.json(result);
     });
 });
 
+/**
+ * @swagger
+ * /orderRequest/controllers/updateOrderRequest:
+ *   put:
+ *     tags:
+ *       - OrderRequest
+ *     description: Updates a single orderRequest
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - in: body
+ *         name: body
+ *         description: orderRequest data that needs to be update
+ *         required: true
+ *         schema:
+ *           $ref: '#/definitions/OrderRequest'
+ *     responses:
+ *       200:
+ *         description: Successfully updated
+ */
 router.put('/controllers/updateOrderRequest', (req, res) => {
     orderRequestsService.updateOrderRequest(req.body, (result) => {
         res.json(result);
     });
 });
 
+/**
+ * @swagger
+ * /orderRequest/controllers/removeOrderRequest/{id}:
+ *   delete:
+ *     tags:
+ *       - OrderRequest
+ *     description: Deletes a single orderRequest
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: id
+ *         description: orderRequest's id
+ *         in: path
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: Successfully deleted
+ */
 router.delete('/controllers/removeOrderRequest/:id', (req, res) => {
     orderRequestsService.removeOrderRequest(req.params.id, (result) => {
         res.send('OrderRequest deleted');
--- src/apis/specialities/specialities.controller.js
+++ src/apis/specialities/specialities.controller.js
@@ -5,6 +5,37 @@ import log from '../../config/log4js.config';
 var specialitiesService = new SpecialitiesService();
 var router = express.Router();
 
+/**
+ * @swagger
+ * definitions:
+ *   Specialities:
+ *     properties:
+ *       id:
+ *         type: string
+ *         format: date
+ *       name:
+ *         type: string
+ */
+/**
+ * @swagger
+ * /specialities/controllers/createSpeciality:
+ *   post:
+ *     tags:
+ *       - Specialities
+ *     description: Creates a new speciality
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: Specialities
+ *         description: Speciality object
+ *         in: body
+ *         required: true
+ *         schema:
+ *           $ref: '#/definitions/Specialities'
+ *     responses:
+ *       200:
+ *         description: Successfully created
+ */
 router.post('/controllers/createSpeciality', (req, res) => {
     specialitiesService.createSpeciality(req.body, (result) => {
         log.info('Speciality created : ' + JSON.stringify(result));
@@ -12,20 +43,95 @@ router.post('/controllers/createSpeciality', (req, res) => {
     res.send('Speciality created successfully');
 });
 
+/**
+ * @swagger
+ * /specialities/controllers/readAllSpecialities:
+ *   get:
+ *     tags:
+ *       - Specialities
+ *     description: Returns all speciality
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: An array of Speciality
+ *         schema:
+ *           $ref: '#/definitions/Specialities'
+ */
 router.get('/controllers/readAllSpecialities', (req, res) => {
     specialitiesService.readAllSpecialities((results) => { res.json(results) });
 });
 
+/**
+ * @swagger
+ * /specialities/controllers/readSpecialityById/{id}:
+ *   get:
+ *     tags:
+ *       - Specialities
+ *     description: Returns Speciality by id
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         description: id for Speciality to return
+ *         required: true
+ *         type: string
+ *         schema:
+ *           $ref: '#/definitions/Specialities'
+ *     responses:
+ *       200:
+ *         description: An Speciality return
+ */
 router.get('/controllers/readSpecialityById/:id', (req, res) => {
     specialitiesService.readSpecialityById(req.params.id, (result) => { log.info(JSON.stringify(result)); });
     res.send('Read Speciality by ID successful');
 });
 
+/**
+ * @swagger
+ * /specialities/controllers/updateSpeciality:
+ *   put:
+ *     tags:
+ *       - Specialities
+ *     description: Updates a single speciality
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - in: body
+ *         name: body
+ *         description: speciality data that needs to be update
+ *         required: true
+ *         schema:
+ *           $ref: '#/definitions/Specialities'
+ *     responses:
+ *       200:
+ *         description: Successfully updated
+ */
 router.put('/controllers/updateSpeciality', (req, res) => {
     specialitiesService.updateSpeciality(req.body, (result) => { log.info('Speciality updated') });
     res.send('Speciality updated successfully');
 });
 
+/**
+ * @swagger
+ * /specialities/controllers/removeSpeciality/{id}:
+ *   delete:
+ *     tags:
+ *       - Specialities
+ *     description: Deletes a single speciality
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: id
+ *         description: speciality's id
+ *         in: path
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: Successfully deleted
+ */
 router.delete('/controllers/removeSpeciality/:id', (req, res) => {
     specialitiesService.removeSpeciality(req.params.id, (result) => { log.info(JSON.stringify(result)); });
     res.send('Speciality deleted');
