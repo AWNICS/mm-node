ea41877720b7c05b0bc90d2ac0bbefb070fd11a3
Arun Gadag <arun.gadag@awnics.com>
2018-08-30 18:04:49 +0530

    updated notifications and visitor appointment modules

--- src/apis/notification/notification.service.js
+++ src/apis/notification/notification.service.js
@@ -1,4 +1,5 @@
 import NotificationDao from './notification.dao';
+import log from '../../config/log4js.config';
 import notificationModel from './index';
 
 const notificationDao = new NotificationDao();
@@ -43,6 +44,25 @@ class NotificationService {
 
     remove(id, callback) {
         notificationDao.delete(id, callback);
+    }
+
+    readWebNotificationsByUserId(userId, page, size, callback) {
+        var offset = ((size * page) - size);
+        notificationModel.notification.findAll({
+            where: {
+                userId: userId,
+                channel: 'web'
+            },
+            offset: offset,
+            limit: size
+        }).then((notifications) => {
+            callback(notifications);
+        }).catch((err) => {
+            log.error('Error while fetching notifications by userId ', err);
+            callback({
+                message: 'Error while fetching notifications'
+            });
+        });
     }
 }
 
