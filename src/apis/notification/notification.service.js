d4346a3c2cd7727501e5f8a38fbaabb26087ccda
Arun Gadag <arun.gadag@awnics.com>
2018-09-07 09:41:27 +0530

    otp module added

--- src/apis/notification/notification.service.js
+++ src/apis/notification/notification.service.js
@@ -1,9 +1,15 @@
+import https from 'https';
+import Dotenv from 'dotenv';
+
 import NotificationDao from './notification.dao';
 import log from '../../config/log4js.config';
 import notificationModel from './index';
 
 const notificationDao = new NotificationDao();
 const Op = require('sequelize').Op;
+const dotenv = Dotenv.config({
+    path: '.env.dev'
+});
 
 class NotificationService {
 
@@ -63,6 +69,84 @@ class NotificationService {
                 message: 'Error while fetching notifications'
             });
         });
+    }
+
+    sendOtp(message, mobileNo, callback) {
+        var options = {
+            "method": "POST",
+            "hostname": "control.msg91.com",
+            "port": null,
+            "path": `/api/sendotp.php?otp_length=${process.env.OTP_LENGTH}&authkey=${process.env.MES91_KEY}&message=${message}&sender=${process.env.MES91_SENDER}&mobile=${mobileNo}&otp_expiry=${process.env.OTP_EXPIRY}`,
+            "headers": {}
+        };
+        var req = https.request(options, function (res) {
+            var chunks = [];
+
+            res.on("data", function (chunk) {
+                chunks.push(chunk);
+            });
+
+            res.on("end", function () {
+                var body = Buffer.concat(chunks);
+                callback(JSON.parse(body));
+            });
+        });
+
+        req.end();
+    }
+
+    resendOtp(mobileNo, callback) {
+        var options = {
+            "method": "POST",
+            "hostname": "control.msg91.com",
+            "port": null,
+            "path": `/api/retryotp.php?authkey=${process.env.MES91_KEY}&mobile=${mobileNo}&retrytype=${process.env.OTP_RETRY_TYPE}`,
+            "headers": {
+                "content-type": "application/x-www-form-urlencoded"
+            }
+        };
+
+        var req = https.request(options, function (res) {
+            var chunks = [];
+
+            res.on("data", function (chunk) {
+                chunks.push(chunk);
+            });
+
+            res.on("end", function () {
+                var body = Buffer.concat(chunks);
+                callback(JSON.parse(body));
+            });
+        });
+
+        req.end();
+    }
+
+    verfiyOtp(mobileNo, otp, callback) {
+        var options = {
+            "method": "POST",
+            "hostname": "control.msg91.com",
+            "port": null,
+            "path": `/api/verifyRequestOTP.php?authkey=${process.env.MES91_KEY}&mobile=${mobileNo}&otp=${otp}`,
+            "headers": {
+                "content-type": "application/x-www-form-urlencoded"
+            }
+        };
+
+        var req = https.request(options, function (res) {
+            var chunks = [];
+
+            res.on("data", function (chunk) {
+                chunks.push(chunk);
+            });
+
+            res.on("end", function () {
+                var body = Buffer.concat(chunks);
+                callback(JSON.parse(body));
+            });
+        });
+
+        req.end();
     }
 }
 
