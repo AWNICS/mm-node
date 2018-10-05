1b802bc63990b5a2467d1a5713be2606b3288633
Hemachandsai <hemachand.atluri@awnics.com>
2018-10-05 11:58:40 +0530

    test

---
+++ src/apis/consultationModes/consultation-modes.model.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, DataTypes) => {
+    var ConsultationModes = sequelize.define('consultation_modes', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING
+        },
+        createdBy: {
+            type: DataTypes.INTEGER,
+            defaultValue: null
+        },
+        updatedBy: {
+            type: DataTypes.INTEGER,
+            defaultValue: null
+        }
+    }, {
+        freezeTableName: true
+    });
+    return ConsultationModes;
+};
